48da2fe673ef063f605c894cc5392c69447ad988
lipiga <lipiga.a2004@gmail.com>
2024-07-22 10:14:54 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null)
 const StoreContextProvider = (props) => {
 
     const [cartItems,setCartItems] = useState({});
-    const url = "http://localhost:4000"
+    const url = "https://mern-stack-main-server.onrender.com"
 
     const [token,setToken] = useState("")
 
